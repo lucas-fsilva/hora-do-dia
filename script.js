a7da78fd3a55339d92d9cf90ea009c5d43a74fc2
Lucas <lucas.fsilva879@gmail.com>
2020-07-22 18:22:24 -0300

    alteraçao js

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-(function load (){
-let msg1 = document.querySelector ('.msg1')
-let msg2 = document.querySelector ('.msg2');
+let msg1 = document.querySelector('.msg1')
+let msg2 = document.querySelector('.msg2');
 let img = document.querySelector('img');
 
-
 let date = new Date();
-let hours = date.getHours();
+//let hours = date.getHours();
+let hours = 20
 msg2.innerHTML = `Agora são ${hours} horas.`
 
 
-if (hours >= 0 && hours < 12){
+if (hours >= 0 && hours < 12) {
     msg1.innerHTML = "Bom dia!"
     img.src = "image/manhã.jpg"
     document.body.style.background = '#edbb53'
-}else if(hours >= 12 && hours < 18){
+} else if (hours >= 12 && hours < 18) {
     msg1.innerHTML = "Boa tarde!"
     img.src = "image/tarde.jpg"
     document.body.style.background = '#f76d02'
-}else{
+} else {
     msg1.innerHTML = "Boa noite!"
     img.src = "image/noite.jpg"
     document.body.style.background = '#715a82'
 }
-})()
